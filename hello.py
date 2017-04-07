ea33a1e330f1f892a1d345fbd759828edec73ca2
duqi <duqi@kongge.com>
2017-04-07 11:48:26 +0800

    基本代码跑通，需要修改css

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+import pandas as pd
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -15,7 +16,11 @@ def index():
 
 @app.route('/<category>')
 def category(category):
-    return render_template('category.html', category=category)
+    web_csv = pd.read_csv(
+        "/Users/duqi/qiusuo/static/website_info.csv", encoding="gb2312")
+    data = web_csv[web_csv['category'] == category]
+    #website = [u'网易', u'京东', u'阿里巴巴', u'杜七']
+    return render_template('category.html', title=category, webcsv=data)
 
 
 @app.errorhandler(404)
