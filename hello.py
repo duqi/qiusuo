65e9bdcfa2c57374253c827cf62c5b7458ec3831
duqi <duqi@kongge.com>
2017-04-07 17:17:57 +0800

    modify css

--- hello.py
+++ hello.py
@@ -19,7 +19,6 @@ def category(category):
     web_csv = pd.read_csv(
         "/Users/duqi/qiusuo/static/website_info.csv", encoding="gb2312")
     data = web_csv[web_csv['category'] == category]
-    #website = [u'网易', u'京东', u'阿里巴巴', u'杜七']
     return render_template('category.html', title=category, webcsv=data)
 
 
